bc5f57438371d7fbd9991e514d624e17a9985ef4
octaviosancho-dev <octaviosanchodev@gmail.com>
2023-10-03 11:25:37 -0300

    Client pages created

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,12 +9,14 @@ import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import { useRouter } from 'next/navigation';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -56,9 +58,18 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className='flex flex-col cursor-pointer'>
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label='My trips' />
-                <MenuItem onClick={() => {}} label='My favorites' />
-                <MenuItem onClick={() => {}} label='My reservations' />
+                <MenuItem
+                  onClick={() => router.push('/trips')}
+                  label='My trips'
+                />
+                <MenuItem
+                  onClick={() => router.push('/favorites')}
+                  label='My favorites'
+                />
+                <MenuItem
+                  onClick={() => router.push('/reservations')}
+                  label='My reservations'
+                />
                 <MenuItem onClick={() => {}} label='My properties' />
                 <MenuItem onClick={rentModal.onOpen} label='Airbnb my home' />
                 <hr />
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -72,7 +72,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
       .then(() => {
         toast.success('Listing reserved!');
         setDateRange(initialDateRange);
-        // Redirect to /trips
+        router.push('/trips');
         router.refresh();
       })
       .catch(() => {
