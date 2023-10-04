7abab1767b052311f68e28d6275c4a891b16ee19
octaviosancho-dev <octaviosanchodev@gmail.com>
2023-10-04 12:46:12 -0300

    Search filters

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,11 +2,27 @@ import prisma from '@/app/libs/prismadb';
 
 export interface IListingsParams {
   userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
 export default async function getListings(params: IListingsParams) {
   try {
-    const { userId } = params;
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      category,
+      locationValue,
+    } = params;
 
     let query: any = {};
 
