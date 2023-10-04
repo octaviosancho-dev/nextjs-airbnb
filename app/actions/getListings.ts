2bdacaa2aecdaf312f24121b621ed52521250c67
octaviosancho-dev <octaviosanchodev@gmail.com>
2023-10-04 13:44:09 -0300

    Error and Loading handle

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -30,6 +30,51 @@ export default async function getListings(params: IListingsParams) {
       query.userId = userId;
     }
 
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount,
+      };
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount,
+      };
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount,
+      };
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate },
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate },
+              },
+            ],
+          },
+        },
+      };
+    }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
